6f41eae92dd33ceb523b382db967abf35b72c5f8
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2020-06-11 16:19:09 +0200

    add doses and cocktails controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.delete_all
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "tomato juice")
+Ingredient.create(name: "ananas juice")
