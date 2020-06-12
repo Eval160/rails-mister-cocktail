ddeddab12429fe0ea116cb8393024b4e6e8d2c98
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2020-06-12 12:47:41 +0200

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,4 +30,54 @@ ingredients.each do |ingredient|
 end
 
 
+
+
+
+# puts "Create new cocktails"
+
+# ("a".."t").each do |letter|
+#   url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}"
+#   drinks = JSON.parse(open(url).read)["drinks"]
+
+#   drinks.each do |drink|
+#     name = drink["strDrink"]
+#     i = 1
+#     str_ingredient = "strIngredient#{i}"
+#     str_measure = "strMeasure#{i}"
+#     ingredients = []
+#     measures = []
+#     until drink[str_ingredient].nil?
+#       ingredient = drink[str_ingredient]
+#       ingredients << ingredient
+#       measure = drink[str_measure]
+#       measures << measure
+#       i += 1
+#       str_ingredient = "strIngredient#{i}"
+#       str_measure = "strMeasure#{i}"
+#     end
+#     cocktail = Cocktail.new(name: name)
+#       j = 0
+#       ingredients.each do |ingredient|
+#         dose = Dose.new(description: measures[j])
+#         if dose.description.nil?
+#         else
+#           ingredient_exist = Ingredient.where(name: ingredient)[0]
+#           if ingredient_exist.nil?
+#             j += 1
+#           else
+#             dose.ingredient = ingredient_exist
+#             dose.cocktail = cocktail
+#             if dose.valid?
+#               dose.save!
+#               j += 1
+#             else
+#               j += 1
+#             end
+
+#           end
+#         end
+#       end
+#   end
+# end
+
 puts "Finish"
