670d3eb05d15a805656e868d5434f2972a99bd25
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2020-06-12 21:16:43 +0200

    add seed with cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 require 'json'
 require 'open-uri'
 
+puts "Delete all cocktails"
+Cocktail.delete_all
 puts "Delete all ingredients"
 Ingredient.delete_all
 # Ingredient.create(name: "lemon")
@@ -20,8 +22,8 @@ Ingredient.delete_all
 # Ingredient.create(name: "tomato juice")
 # Ingredient.create(name: "ananas juice")
 
-url = url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-ingredients = JSON.parse(open(url).read)["drinks"]
+url_ingregients = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url_ingregients).read)["drinks"]
 
 puts "Create new ingredients"
 ingredients.each do |ingredient|
@@ -31,53 +33,49 @@ end
 
 
 
+puts "Create new cocktails"
 
+("a".."t").each do |letter|
+  url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}"
+  drinks = JSON.parse(open(url).read)["drinks"]
 
-# puts "Create new cocktails"
+  drinks.each do |drink|
+    name = drink["strDrink"]
+    cocktail = Cocktail.new(name: name)
+    cocktail.save!
+    i = 1
 
-# ("a".."t").each do |letter|
-#   url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}"
-#   drinks = JSON.parse(open(url).read)["drinks"]
+    ingredients = []
+    measures = []
+    until drink["strIngredient#{i}"].nil?
+      ingredient = drink[ "strIngredient#{i}"]
+      ingredients << ingredient
+      measure = drink["strMeasure#{i}"]
+      measures << measure
+      i += 1
+    end
+          j = 0
+      ingredients.each do |ingredient|
+        dose = Dose.new(description: measures[j])
+        if dose.description.nil?
+        else
+          ingredient_exist = Ingredient.where(name: ingredient)[0]
+          if ingredient_exist.nil?
+            j += 1
+          else
+            dose.ingredient = ingredient_exist
+            dose.cocktail = cocktail
+            if dose.valid?
+              dose.save!
+              j += 1
+            else
+              j += 1
+            end
 
-#   drinks.each do |drink|
-#     name = drink["strDrink"]
-#     i = 1
-#     str_ingredient = "strIngredient#{i}"
-#     str_measure = "strMeasure#{i}"
-#     ingredients = []
-#     measures = []
-#     until drink[str_ingredient].nil?
-#       ingredient = drink[str_ingredient]
-#       ingredients << ingredient
-#       measure = drink[str_measure]
-#       measures << measure
-#       i += 1
-#       str_ingredient = "strIngredient#{i}"
-#       str_measure = "strMeasure#{i}"
-#     end
-#     cocktail = Cocktail.new(name: name)
-#       j = 0
-#       ingredients.each do |ingredient|
-#         dose = Dose.new(description: measures[j])
-#         if dose.description.nil?
-#         else
-#           ingredient_exist = Ingredient.where(name: ingredient)[0]
-#           if ingredient_exist.nil?
-#             j += 1
-#           else
-#             dose.ingredient = ingredient_exist
-#             dose.cocktail = cocktail
-#             if dose.valid?
-#               dose.save!
-#               j += 1
-#             else
-#               j += 1
-#             end
-
-#           end
-#         end
-#       end
-#   end
-# end
+          end
+        end
+      end
+  end
+end
 
 puts "Finish"
