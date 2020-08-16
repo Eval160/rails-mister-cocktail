9e80f50ca649e8f2fec468ed96d608ed7ac0255b
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2020-08-16 14:06:45 +0200

    delete blank

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -3,6 +3,7 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
+
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
